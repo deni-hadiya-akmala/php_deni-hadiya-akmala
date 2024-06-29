f1630e9d52292ca913d7b34f25f6860ab636f7c0
unknown <denihadiya@gmail.com>
2024-06-29 09:19:52 +0700

    soal 1 sudah

--- soal1.php
+++ soal1.php
@@ -1,24 +1,23 @@
 <?php
-
-
 $jml = $_GET['jml'];
 
 echo "<table border=1>\n";
 
-// Looping dari nilai 'jml' hingga 1 untuk membuat baris tabel
-for ($a = $jml; $a > 0; $a--)
-{
-  echo "<tr>\n";
+for ($a = $jml; $a > 0; $a--) {
+    // Menghitung total untuk baris ini
+    $total = 0;
+    for ($b = $a; $b > 0; $b--) {
+        $total += $b;
+    }
+    // Menambahkan baris total sebelum baris angka
+    echo "<tr><td colspan=\"$a\">TOTAL: $total</td></tr>\n";
 
-  // Looping dari nilai baris saat ini hingga 1 untuk membuat kolom dalam baris tersebut
-  for ($b = $a; $b > 0; $b--)
-  {
-    echo "<td>$b</td>";
-  }
- 
-  echo "</tr>\n";
+    echo "<tr>\n";
+    // Looping dari nilai baris saat ini hingga 1 untuk membuat kolom dalam baris tersebut
+    for ($b = $a; $b > 0; $b--) {
+        echo "<td>$b</td>";
+    }
+    echo "</tr>\n";
 }
 
 echo "</table>";
-
-?>
